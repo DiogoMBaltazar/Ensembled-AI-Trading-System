0e7019cc19754f08b09f477c5059f03abf9d0f89
DiogoMBaltazar <diogobal@sapo.pt>
2021-06-20 19:07:55 +0100

    Web scraper Interactive Brokers website

--- Utilities/webScrapingIBSite.py
+++ Utilities/webScrapingIBSite.py
@@ -1,8 +1,16 @@
 import requests
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+
 import json
 import pandas as pd
 from bs4 import BeautifulSoup
 import re
+
+from numpy import savetxt  
+
+import time
+
 
 # Script returns a dictionary with all the NASDAQ Equities listed on the IB website.
 # Later, will be updated to retrieve futures as well.
@@ -18,10 +26,20 @@ end_name = "</a"
 tickers = []
 names = []
 
+s = requests.Session()
+
+retries = Retry(total=5,
+                backoff_factor=0.1,
+                status_forcelist=[ 500, 502, 503, 504 ])
+
+s.mount('http://', HTTPAdapter(max_retries=retries))
+
 for page_idx in range(1, end_page + 1):
-    URL = requests.get ('https://www.interactivebrokers.com/en/index.php?f=2222&exch=nasdaq&showcategories=STK&p=&cc=&limit=100&page={}'.format(page_idx))
+    URL = s.get ('https://www.interactivebrokers.com/en/index.php?f=2222&exch=nasdaq&showcategories=STK&p=&cc=&limit=100&page={}'.format(page_idx))
+    time.sleep(1)
     soup = BeautifulSoup(URL.text, 'html.parser')
     stocks_table = soup.find_all('table', class_ = 'table table-striped table-bordered')
+    time.sleep(1)
     stocks_info = stocks_table[2]('td')
     for i, j in zip(range(0, 396, 4), range(1, 397, 4)):
         stocks_ticker = str(stocks_info[i])
@@ -32,6 +50,8 @@ for page_idx in range(1, end_page + 1):
         names.append(result_name)
 
     print('Extracting page', page_idx)
+
+savetxt("ib_stocks_listings.csv", zip(tickers, names), delimiter=",")
 
 output = {}
 for key in tickers:
