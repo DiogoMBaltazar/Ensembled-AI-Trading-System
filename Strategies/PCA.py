344303b467ea53774dcf9aed5d9db6b56f979d5d
DiogoMBaltazar <diogobal@sapo.pt>
2021-08-18 12:12:36 +0100

    Principal Component Analysis

--- Strategies/PCA.py
+++ Strategies/PCA.py
@@ -2,10 +2,8 @@ import sys
 
 sys.path.insert(0, r'F:\Ensembled-AI-Trading-System')
 
-import time
 import numpy as np
 import pandas as pd 
-from datetime import date
 from Utilities.config import *
 from sklearn.preprocessing import *
 from sklearn.decomposition import *
@@ -49,12 +47,18 @@ class PCA_class(object):
         n_pcs= self.pca.n_components_ # get number of component
         
         # get the most important feature names
+        
+        # https://stackoverflow.com/questions/22984335/recovering-features-names-of-explained-variance-ratio-in-pca-with-sklearn
         initial_feature_names = self.data.columns
         most_important = [np.abs(self.pca.components_[i]).argmax() for i in range(n_pcs)]
         most_important_names = [initial_feature_names[most_important[i]] for i in range(n_pcs)]
-        print("Features after PCA:", most_important_names[:25])
-        print("|-----------------------------------------------------------|")
-        print("|-----------------------------------------------------------|")
+        
+        dic = {'PC{}'.format(i+1): most_important_names[i] for i in range(n_pcs)}
+        df = pd.DataFrame(sorted(dic.items()))
+
+        print("The important features are the ones that influence more the components and thus, \nHave a large absolute value/coefficient/loading on the component.:", df)
+        print("|------------------------------------------------------------------------|")
+        print("|------------------------------------------------------------------------|")
 
         #column names for new reduced dataframe
         self.new_normalized.columns = most_important_names
@@ -64,5 +68,7 @@ class PCA_class(object):
         self.new_normalized['crypto_id'] = self.crypto_id
 
         self.new_normalized = self.new_normalized.loc[:,~self.new_normalized.columns.duplicated()]
+        print(f"Initial dataframe shape {self.data.shape} --> After PCA: {self.new_normalized.shape}")
+        print("|-----------------------------------------------------------|")
 
         return self.new_normalized
