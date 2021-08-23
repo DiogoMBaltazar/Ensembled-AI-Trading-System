1c357ec38e38fb3f4953fb2c50a549a6c829bea0
DiogoMBaltazar <diogobal@sapo.pt>
2021-08-24 00:20:51 +0100

    fourier_transformation

--- Strategies/FeatureEngineering.py
+++ Strategies/FeatureEngineering.py
@@ -1,12 +1,9 @@
 import sys
 sys.path.insert(0, r'F:\Ensembled-AI-Trading-System')
 
-import time
 import warnings
-import datetime
 import numpy as np
 import pandas as pd
-from datetime import date
 from ta.utils import *
 from ta import add_all_ta_features
 from Utilities.Database_funcs import *
@@ -65,7 +62,6 @@ class FeatureEngineering(object):
 
     def more_features(self, output):
 
-
         # Needs adjustment according data granularity
         output['Cumulative_return_100'] = ((output['close'] / output['close'].iloc[0]) - 1) * 100
         output['Daily_return_100'] = ((output['close'] / output['close'].shift(1)) - 1) * 100
@@ -80,6 +76,18 @@ class FeatureEngineering(object):
         return output
 
 
+    def volume(self, output):
+
+
+        # for 1 min tick data
+        output['15 min volume'] = output['volume'].rolling(window=15).sum()
+        output['30 min volume'] = output['volume'].rolling(window=30).sum()
+        output['60 min volume'] = output['volume'].rolling(window=60).sum()
+        output['6 hr volume'] = output['volume'].rolling(window=360).sum()
+        output['12 hr volume'] = output['volume'].rolling(window=720).sum()
+        output['24 hr volume'] = output['volume'].rolling(window=1440).sum()
+
+
     def NLP_Features(self, data):
 
         df = pd.read_csv(r'F:\Ensembled-AI-Trading-System\data\Sentiment_Analysis_date.csv')
@@ -91,5 +99,43 @@ class FeatureEngineering(object):
         print(df.columns)
 
 
+    def fourier_transformation(self, data):
+
+        # https://github.com/manganganath/stock_price_trend_fft
+
+        close_fft = np.fft.fft(np.asarray(data['Close'].tolist()))
+        fft_df = pd.DataFrame({'fft':close_fft})
+        fft_df['absolute'] = fft_df['fft'].apply(lambda x: np.abs(x))
+        fft_df['angle'] = fft_df['fft'].apply(lambda x: np.angle(x))
+
+
+        # Low pass filtering 
+        # take only 200 components
+
+        fft_list = np.asarray(fft_df['fft'].tolist())
+        fft_list[100:-100] = 0
+
+        return fft_list
+
 # # calculate the volume in coin from QUANTITY in USDT (default)
 # volume = float(quantity / float(last_price))
+
+
+# average buying price from bithumb.com – you can call fetch_my_trades, 
+# filter the trades by the side (buy/sell) 
+# and then aggregate 
+# and divide them by the sum of their volumes.
+
+
+
+# log_ret_dif = crypto_data['log_returns'] > crypto_data['log_returns'].shift(1)
+
+# if log_ret_dif > 10% std of normal distribution of all log_returns:
+#     df['target'] = 'buy'
+# elif 10% < log_ret_dif < 25%:
+#     df['target'] = 'hold'
+# else:
+#     df['target'] = 'sell'
+
+
+# https://github.com/ThirstyScholar/trading-bitcoin-with-reinforcement-learning/blob/master/frl/data.py
