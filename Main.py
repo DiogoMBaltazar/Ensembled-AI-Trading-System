9e921671ae51825037407cdd8bc5039bb721ad66
DiogoMBaltazar <60813994+DiogoMBaltazar@users.noreply.github.com>
2021-09-07 15:16:36 +0100

    Update to support RL

--- Main.py
+++ Main.py
@@ -5,9 +5,11 @@ from Strategies.PCA import *
 from Utilities.config import *
 from Utilities.util_funcs import *
 from Strategies.AutoGluon import *
+
 from Strategies.RandomForest import *
 from Utilities.Database_funcs import *
 from Strategies.FeatureEngineering import *
+from Strategies.Reinforcement_Learning import *
 from Strategies.Facebook_Prophet import FBProphet
 
 begin_time = datetime.datetime.now()
@@ -28,6 +30,8 @@ def system_start():
 
     AML = AutoML()
     
+    RL = ReinforcementLearning()
+    
     crypto_data = Crypto_DB.get_symbol_ohlcv("ETH/USDT")
     data_01 = Features.handling_ohlcv_and_TA_indicators(crypto_data)
     data_02 = Features.log_returns(data_01)
@@ -38,21 +42,22 @@ def system_start():
 
     #ensemble the below models eventually
     data_05 = pca_model.pca(data_04)
-    print(data_05.columns)
-    time.sleep(15)
-    data_06 = AML.AML_model(data_05)
 
-    prophet_pred = FBP.Prophet_model(crypto_data)
+    rl_data = RL.rl_agents(data_05)
+    
+    #time.sleep(15)
+    #data_06 = AML.AML_model(data_05)
+
+    # prophet_pred = FBP.Prophet_model(crypto_data)
 
     # rf = RF.rf_model(crypto_data)
 
     end_time = datetime.datetime.now() - begin_time
     print('\033[94m' + f'This script took %s', end_time, "seconds to run")
 
-    return data_06, prophet_pred
-    print(crypto_data)
-    crypto_data = AML.AML_model(crypto_data)
+    # crypto_data = AML.AML_model(crypto_data)
     
-    print(crypto_data)
+    return rl_data
+
 
 system_start()
